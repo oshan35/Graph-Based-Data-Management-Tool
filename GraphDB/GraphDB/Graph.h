c9321029ecc855177cd1cbf183f120180d35d1b3
oshan devinda <oshan.devinda35@gmail.com>
2022-12-16 21:19:21 +0530

    implimentation of graph class

--- GraphDB/GraphDB/Graph.h
+++ GraphDB/GraphDB/Graph.h
@@ -1,25 +1,56 @@
 #pragma once
 #include<iostream>
 #include "Node.h"
+#include <queue>
+#include <unordered_set>
 using namespace std;
 
 class Graph
 {
 	Node* indexNode;
+	int numOfNodes;
 
 public:
 	Graph(Node* indexNode) {
 		this->indexNode = indexNode;
+		numOfNodes = 1;
 	}
 
 	void insertNode(Node* prevNode, Node* node, int index) {
 		node->addInRelation(index, prevNode);
+
 		prevNode->addOutRelation(index, node);
+
+		numOfNodes++;
 	}
 
-	Node* searchNode(string data) {
+	void searchNode() {
+		Node* start = indexNode;
+		queue<Node*> q;
+
+		unordered_set<Node*> visited;
+
+		q.push(start);
+
+		visited.insert(start);
+
+		while (!q.empty()) {
+			Node* curr = q.front();
+			q.pop();
+			map<int, Node*> outmap;
+
+			for (auto i = outmap.begin(); i != outmap.end(); i++) {
+				if (!visited.count(i->second)) {
+					q.push(i->second);
+					visited.insert(i->second);
+				}
+			}
+				
+		}
 
 
+
+		
 	}
 
 
