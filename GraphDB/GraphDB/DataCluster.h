f2f52551ccc7cd38182080a8dbfadf5e425b88fe
oshan devinda <oshan.devinda35@gmail.com>
2022-12-22 21:17:24 +0530

    completed the methods required to create the data cluster

--- GraphDB/GraphDB/DataCluster.h
+++ GraphDB/GraphDB/DataCluster.h
@@ -5,17 +5,14 @@
 #include <vector>
 #include<algorithm>
 #include<variant>
-
-#include<vector>
-#include<iostream>
-#include "Node.h"
-#include<variant>
 #include <map>
+#include "Tree.h"
+#include "Graph.h"
 using namespace std;
 class DataCluster
 {
-	vector<Node*> tree;
-	Node* graph;
+	vector<Tree> coulmnTrees;
+	Graph graph;
 
 	vector<vector<Node*>> createNodes(vector < vector<variant<int, double, string>>> rows, vector<string> firstrow) {// should first row be given seperately
 		for (int i = 0; i < rows[0].size(); i++) {
@@ -40,7 +37,53 @@ class DataCluster
 			}
 		}
 	}
-public:
+
+
+
+	void createTrees(vector<vector<Node*>> nodeRawData){
+		for (int col = 0; col < nodeRawData.at(0).size(); col++)
+		{
+
+			vector<Node*> nodeCol;
+			for (int row = 0; row < nodeRawData.size(); row++)
+			{
+				nodeCol.push_back(nodeRawData[col][row]);
+			}
+
+			Tree newTree;
+			newTree.createTree(nodeCol);
+
+			coulmnTrees.push_back(newTree);
+		}
+		
+	}
+
+	void createGraph(vector<vector<Node*>> nodeRawData){
+		Node* indexNode;
+		for(int row=0; row < nodeRawData.size(); row++){
+			Node* prev = nullptr;
+			for(int col=0; col < nodeRawData[0].size(); col++){
+				Node* curr = nodeRawData[row][col];
+				if(col == 0){
+					indexNode->addOutRelation(row,curr);
+					prev = curr;
+				}else{
+					graph.insertNode(prev,curr,row);
+					prev = curr;
+				}
+			}
+		}
+	}
+
+	public:
+	void createDataCluster(vector < vector<variant<int, double, string>>> rows, vector<string> firstrow){
+		vector<vector<Node*>> rowNodeData = createNodes(rows, firstrow);
+		createTrees(rowNodeData);
+		createGraph(rowNodeData);
+	}
+
+
+
 
 };
 
