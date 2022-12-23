6667db7730212a0fa2f7ba5f7fb6774ff8869c46
oshan devinda <oshan.devinda35@gmail.com>
2022-12-23 11:17:43 +0530

    claster manager class

---
+++ GraphDB/GraphDB/DataClusterManager.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<map>
+#include<vector>
+#include"DataCluster.h"
+using namespace std;
+
+class DataClusterManager{
+    vector<DataCluster> dataClusters;
+
+    public:
+    vector < vector<variant<int, double, string>>> convertRawData(map<int, vector<string>> rawData){
+
+    }
+
+    void createDataCluster(map<int, vector<string>> datamap){
+
+    }
+}
